1110c3a2c709b8ae00de244146376847b8e649b1
Ndonga <ndonga.edwin@gmail.com>
2022-06-10 12:21:05 +0300

    initial commit

---
+++ src/main/java/com/example/springboothelloworld/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+    @GetMapping()
+    public String printHelloWorld(){
+        return "Spring Boot Hello world application!";
+    }
+}
